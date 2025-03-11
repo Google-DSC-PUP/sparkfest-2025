63abacbf597e46db7866561f3c2f35d7afc4454a
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2025-03-11 16:19:13 +0800

    style: responsiveness of the landing page

--- src/components/features/LandingPage.tsx
+++ src/components/features/LandingPage.tsx
@@ -25,54 +25,61 @@ export default function LandingPage() {
 
   return (
     <div
-      className="relative flex w-full flex-col md:flex-row items-center justify-center lg:justify-around min-h-screen bg-cover bg-center"
+      className="relative flex w-full p-4 flex-col-reverse lg:flex-row items-center justify-center gap-4 min-h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${LandingPageImage})` }}
     >
-      {/* Left side with mascot */}
-      <div className="w-fit flex justify-center md:justify-start z-10">
-        <img src={SparkyImage} alt="sparky image" />
+      {/* Left side with mascot - 50% width */}
+      <div className="w-full xl:w-1/2 flex justify-center z-10">
+        <img
+          src={SparkyImage}
+          alt="sparky image"
+          className="w-[80%] max-w-[400px] md:max-w-[500px] lg:w-full"
+        />
       </div>
 
-      {/* Right side with countdown and text */}
-      <div className="flex w-full flex-col items-start gap-2 text-white z-10">
+      {/* Right side with countdown and text - 50% width */}
+      <div className="w-full xl:w-1/2 flex flex-col items-center lg:items-start gap-2 text-white z-10">
         {/* Countdown boxes with NumberTicker */}
         <div className="flex flex-col items-start gap-4">
-          <div className="flex items-center justify-center gap-4 cursor-pointer ">
-            <div className="flex h-40 sm:h-60 md:h-80 px-4 items-center justify-center rounded-[64px] border-2 border-white/70 hover:bg-blue text-6xl sm:text-8xl md:text-[21.056rem] font-mono font-bold backdrop-blur-sm ">
+          <div className="flex items-center justify-center gap-4 cursor-pointer">
+            <div
+              className="flex px-4 items-center justify-center rounded-[40px] lg:rounded-[64px] border-2 border-white/70 hover:bg-blue 
+  text-[12rem] sm:text-[14rem] md:text-[16rem] lg:text-[18rem] xl:text-[21.056rem] font-mono font-bold leading-none backdrop-blur-sm"
+            >
               <NumberTicker
                 className="text-white"
                 value={parseInt(daysArray[0])}
               />
             </div>
-            <div className="flex h-40 sm:h-60 md:h-80 px-4 items-center justify-center rounded-[64px] border-2 border-white/70 hover:bg-yellow text-6xl sm:text-8xl md:text-[21.056rem] font-mono font-bold backdrop-blur-sm">
+
+            <div
+              className="flex px-4 items-center justify-center rounded-[40px] lg:rounded-[64px] border-2 border-white/70 hover:bg-yellow 
+  text-[12rem] sm:text-[14rem] md:text-[16rem] lg:text-[18rem] xl:text-[21.056rem] font-mono font-bold leading-none backdrop-blur-sm"
+            >
               <NumberTicker
                 className="text-white"
                 value={parseInt(daysArray[1])}
               />
             </div>
           </div>
-          <div className="text-center text-lg sm:text-xl md:text-2xl title2">
-            DAYS TO GO
-          </div>
+          <div className="title2">DAYS TO GO</div>
         </div>
 
-        <h1 className="text-center text-2xl sm:text-4xl md:text-6xl lg:text-7xl header [text-shadow:0px_4.56px_4.56px_#A84038]">
+        <h1 className="header [text-shadow:0px_4.56px_4.56px_#A84038]">
           <span className="text-white">SPARK</span>
-          <span className="text-orange-400">F</span>
-          <span className="text-blue-400">E</span>
-          <span className="text-green-400">S</span>
-          <span className="text-red-400">T</span>
+          <span className="text-yellow">F</span>
+          <span className="text-blue">E</span>
+          <span className="text-green">S</span>
+          <span className="text-red">T</span>
           <span className="text-white">:</span>
         </h1>
 
-        <h2 className="text-center text-lg sm:text-xl md:text-3xl header2">
-          “Festival of Sparks”
-        </h2>
+        <h2 className="header2">“Festival of Sparks”</h2>
       </div>
 
       <RetroGrid
         angle={75}
-        cellSize={40}
+        cellSize={35}
         opacity={0.1}
         lightLineColor="rgba(241, 243, 244, 0.64)"
         darkLineColor="rgba(241, 243, 244, 0.64)"
