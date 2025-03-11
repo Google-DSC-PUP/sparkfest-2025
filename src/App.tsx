13267eb07357095db59a8519e97a72eedf1e4ca9
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2025-03-11 16:20:36 +0800

    feat: added the whole gradient for the whole page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import LandingPage from "./components/features/LandingPage";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+    <div className="relative custom-gradient flex flex-col ">
+      <LandingPage />
     </div>
   );
 }
