43e1349bb49a3d5a7ca8f6a5b0355d47806c52e9
mgachiee <bobadilla.markallen49@gmail.com>
2025-03-12 19:57:23 +0800

    feat: sponsor section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import LandingPage from "./components/features/LandingPage";
+import SponsorsSection from "./components/Sponsors/SponsorsSection";
 
 function App() {
   return (
     <div className="relative custom-gradient flex flex-col ">
       <LandingPage />
+      <SponsorsSection />    
     </div>
   );
 }
