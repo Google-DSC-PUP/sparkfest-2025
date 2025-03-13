99acce85bc5ddbbac894569ae9adf84812c44759
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-03-13 19:08:10 +0800

    feature (about) - add custom gradient for the about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import About from "@/components/features/About";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <About />
     </div>
   );
 }
--- src/components/features/About.tsx
+++ src/components/features/About.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { SDGData } from "@/data/dataLoader";
 
 const About = () => {
   return (
-    <div>
+    <div className="h-[940px] w-full custom-gradient-about-2">
       
     </div>
-  )
-}
+  );
 
-export default About
+};
+
+export default About;
