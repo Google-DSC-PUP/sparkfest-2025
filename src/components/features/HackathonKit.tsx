b26afab5f57781f8a04dfdf9dd2ca94f17ff7d85
Phisbol <stan.magallon@gmail.com>
2025-03-13 18:40:57 +0800

    feat: enhance HackathonKit component with background gradient and descriptive text

--- src/components/features/HackathonKit.tsx
+++ src/components/features/HackathonKit.tsx
@@ -4,10 +4,11 @@ import React from "react";
 
 
 const HackathonKit = () => {
-    return <div className="w-full h-[100vh] flex flex-row">
+    return <div className="w-full h-[100vh] flex flex-row bg-gradient-to-b from-fuchsia-900 to-blue-900">
             <div className="w-[50vw] h-[100vh] flex justify-center items-center">
                 <div className="w-[90%] h-[45vw]">
                     <h1 className="[text-shadow:_0_4.56px_4.56px_#A84038] leading-[150px] text-white header">HACKATHON <span className="text-[var(--color-yellow)]">K</span><span className="text-[var(--color-blue)]">I</span><span className="text-[var(--color-green)]">T</span><img src="src/assets/IsoSparky-Head-BASE.png" className="aspect-1/1 w-[147px] inline"></img></h1>
+                    <p className="body1 text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
             </div>
             <div className="w-[50vw] flex justify-center align-center height-[80vh] ">
