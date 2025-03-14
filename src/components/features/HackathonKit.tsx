549f24e265d18ab60237b21a9fecdda1737a6a83
Phisbol <stan.magallon@gmail.com>
2025-03-14 18:19:08 +0800

    feat: adjust layout and alignment in HackathonKit component for improved responsiveness

--- src/components/features/HackathonKit.tsx
+++ src/components/features/HackathonKit.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 
 
 const HackathonKit = () => {
-    return <div className="w-full h-[100vh] flex flex-col md:flex-row bg-gradient-to-b from-fuchsia-900 to-blue-900">
+    return <div className="w-full h-[100vh] flex flex-col md:flex-row bg-gradient-to-b from-fuchsia-900 to-blue-900 md:justify-around">
             <div className="w-[100%] text-center  md:w-[50vw] md:text-left h-[100vh] flex justify-center items-center">
                 <div className="w-[100%] md:w-[90%] h-[45vw] ">
                     <h1 className="[text-shadow:_0_4.56px_4.56px_#A84038] leading-[150px] text-white header">HACKATHON<br></br> <span className="text-[var(--color-yellow)]">K</span><span className="text-[var(--color-blue)]">I</span><span className="text-[var(--color-green)]">T</span><img src="src/assets/IsoSparky-Head-BASE.png" className="hidden aspect-1/1 w-[147px] md:inline"></img></h1>
@@ -14,9 +14,9 @@ const HackathonKit = () => {
                     <Button>hi</Button>
                 </div>
             </div>
-            <div className="w-[50vw] flex justify-center align-center height-[80vh] ">
-                <div className="w-[90%] h-[100vh] flex justify-center align-center">
-                    <img src="src/assets/Artboard 1 1.png" className="hidden md:aspect-square w-[45vw] h-[45vw] self-center rounded-[20px] border-solid border-white border-[5px] md:block"></img>
+            <div className="w-[45vw] flex justify-center align-center height-[80vh] ">
+                <div className="w-[100%] h-[100vh] flex justify-center align-center">
+                    <img src="src/assets/Artboard 1 1.png" className="hidden md:aspect-square w-[45vw] h-[45vw] self-center rounded-[20px] border-solid border-white border-[5px] md:block "></img>
                 </div>
             </div>
         </div>
