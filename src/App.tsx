869a3e1ba58abf8db0e8b43c8826729002d06b18
reyes-simonejake <reyesjake262@gmail.com>
2025-03-14 08:31:13 +0800

    feat: upload the program for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import Navbar from "@/components/features/Navbar";
+import backgroundImage from "@/assets/landing-page-image.png"; // Import the image
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <div
+        className="flex flex-col items-center justify-center min-h-svh w-full bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Navbar />
+        <Button>Click me</Button>
+      </div>
+    </BrowserRouter>
   );
 }
 
