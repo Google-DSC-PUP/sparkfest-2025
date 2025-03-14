e06557099d808e493cc2acaf6e339f4cc49ab58e
Phisbol <stan.magallon@gmail.com>
2025-03-14 17:12:31 +0800

    feat: improve text alignment and responsiveness in HackathonKit component

--- src/components/features/HackathonKit.tsx
+++ src/components/features/HackathonKit.tsx
@@ -10,7 +10,7 @@ const HackathonKit = () => {
                 <div className="w-[100%] md:w-[90%] h-[45vw] ">
                     <h1 className="[text-shadow:_0_4.56px_4.56px_#A84038] leading-[150px] text-white header">HACKATHON<br></br> <span className="text-[var(--color-yellow)]">K</span><span className="text-[var(--color-blue)]">I</span><span className="text-[var(--color-green)]">T</span><img src="src/assets/IsoSparky-Head-BASE.png" className="hidden aspect-1/1 w-[147px] md:inline"></img></h1>
                     <img src="src/assets/Artboard 1 1.png" className="justify-self-center aspect-square w-[70vw] h-[70vw] self-center rounded-[20px] border-solid border-white border-[5px] md:hidden sm:block"></img>
-                    <p className="body1 text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p className="body1 text-white max-md:w-[70%] max-md:text-center max-md:mx-[15%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     <Button>hi</Button>
                 </div>
             </div>
