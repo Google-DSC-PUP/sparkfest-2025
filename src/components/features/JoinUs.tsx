934df059620c8db9a4a15c2abefbdaf9805edebd
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-03-16 20:42:33 +0800

    feat (join-us): integrate JoinUs component with responsive design and particle effects

--- src/components/features/JoinUs.tsx
+++ src/components/features/JoinUs.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import RBlockSet from '/images/R-blockset.png'
 import LBlockSet from '/images/L-blockset.png'
+import TBlockSet from '/images/T-blockset.png'
+import BBlockSet from '/images/B-blockset.png'
 import { Particles } from '../magicui/particles'
 import PixelButton from '../ui/pixelated-btn'
 
@@ -9,7 +11,7 @@ const JoinUs = () => {
     <div className='relative flex justify-center items-center w-full h-[967px]'>
         {/* Small particles */}
         <Particles
-            className="absolute inset-0"
+            className="absolute inset-0 -z-10"
             quantity={80}  // Fewer particles
             staticity={30}  // Slower movement
             ease={10}  // Smooth animation
@@ -18,7 +20,7 @@ const JoinUs = () => {
         
         {/* Medium particles */}
         <Particles
-            className="absolute inset-0"
+            className="absolute inset-0 -z-10"
             quantity={100}  // Fewer particles
             staticity={30}  // Slower movement
             ease={10}  // Smooth animation
@@ -34,16 +36,24 @@ const JoinUs = () => {
             size={5}  // Bigger particles
         />
 
-        {/* images */}
-        <div className='absolute right-0 '>
-            <img src={RBlockSet} alt="" />
+        {/* Image - Desktop */}
+        <div className='hidden lg:block absolute right-0'>
+            <img src={RBlockSet} alt="" className='opacity-80' />
         </div>
-        <div className='absolute left-0'>
-            <img src={LBlockSet} alt="" />
+        <div className='hidden lg:block absolute left-0'>
+            <img src={LBlockSet} alt="" className='opacity-80' />
         </div>
+
+
 
         {/* banner */}
         <div className=''>
+
+            {/* top-blockset mobile */}
+            <div className='block lg:hidden mb-16 mt-20'>
+                <img src={TBlockSet} alt="" className='opacity-80 w-full p-4' />
+            </div>
+
             <div className='header text-center text-white' style={{textShadow: "0px 4px 4px #A84038"}}>
                 April {'  '}
                 <span className="text-yellow">2</span>
@@ -51,11 +61,16 @@ const JoinUs = () => {
                     <span className='text-green'>2</span>
                     <span className='text-red'>5</span>
             </div>
-            <div className='text-center title2 mt-12 text-white'>
+            <div className='text-center title2 mt-4 lg:mt-12 text-white'>
                 Join ipsum dolor sit amet!
             </div>
-            <div className='flex justify-center mt-12'>
-                <PixelButton color='#4CAF50' classname='w-60'>Join Now</PixelButton>
+            <div className='flex justify-center mt-8 lg:mt-12'>
+                <PixelButton color='#4CAF50' classname='w-60'>Register now</PixelButton>
+            </div>
+
+            {/* bottom-blockset mobile */}
+            <div className='block lg:hidden mt-16 mb-20'>
+                <img src={BBlockSet} alt="" className='opacity-80 w-full p-4' />
             </div>
       </div>
     </div>
